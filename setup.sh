8f8ad985bfbdf3e4d33bf6f0b683aa5ecbe02591
John Rehbein <john.rehbein@icloud.com>
2020-06-15 18:12:37 -0700

    Setup script should work now

--- setup.sh
+++ setup.sh
@@ -5,15 +5,16 @@
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p MyROMs;
 mkdir -p MyCores;
+mkdir -p Binaries/Win64;
 
-wget https://buildbot.libretro.com/nightly/windows/x86_64/redist.7z --directory /tmp;
+wget -c https://buildbot.libretro.com/nightly/windows/x86_64/redist.7z --directory /tmp;
 
 mkdir -p libretro;
 
 pacman -S --noconfirm --needed p7zip;
 7z x -aoa -o./libretro /tmp/redist.7z;
 
-wget https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip --directory /tmp;
+wget -c https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip --directory /tmp;
 7z x -aoa -o/tmp /tmp/SDL2-devel-2.0.12-VC.zip
 cp -a /tmp/SDL2-2.0.12/include/. ./Source/UnrealLibretro/Public/SDL2
 cp -a /tmp/SDL2-2.0.12/lib/x64/. ./Binaries/Win64
