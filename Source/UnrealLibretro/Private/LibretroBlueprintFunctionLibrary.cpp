6d4eb659d4c9d1a482ffc3a5f4bc0116cf5bf598
John Rehbein <john.rehbein@icloud.com>
2023-06-06 19:37:46 -0700

    Fix packaging with UAT on UE 5.0

--- Source/UnrealLibretro/Private/LibretroBlueprintFunctionLibrary.cpp
+++ Source/UnrealLibretro/Private/LibretroBlueprintFunctionLibrary.cpp
@@ -5,6 +5,7 @@
 #include "Engine/Engine.h"
 #include "Camera/CameraComponent.h"
 #include "PhysicsEngine/PhysicsSettings.h"
+#include "Runtime/Launch/Resources/Version.h"
 
 #if    ENGINE_MAJOR_VERSION != 5 \
     || ENGINE_MINOR_VERSION <  1
