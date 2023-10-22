2cf13526f6fee54dfff837e8e6644ce051afd773
John Rehbein <john.rehbein@icloud.com>
2023-10-21 19:49:13 -0700

    Modify to package in parent directory
    - UAT copies every file unconditionally in your plugin directory when
    building the plugin. This would lead to quadratic io with respect to
    number of plugins being packaged

--- package.py
+++ package.py
@@ -25,7 +25,7 @@ def get_unreal_version(engine_install_directory="C:/Program Files/Epic Games/UE_
         else:
             raise Exception("Could not find Unreal Engine version in 'Build.version'")
 
-def git_get_tag_or_short_hash(revision="HEAD"):
+def git_get_version_name(revision="HEAD"):
     try:
         # If on a tagged commit, this will return just the tag
         # If not on a tagged commit, this will return <last tag>-<rev>-<short hash>
@@ -44,14 +44,18 @@ def git_get_tag_or_short_hash(revision="HEAD"):
     return result
 
 if __name__ == "__main__":
-    # This logic in here is a little confusing because we package the plugin as UnrealLibretro
-    # in folder UnrealLibretro so the packaged plugin is at UnrealLibretro/UnrealLibretro
     plugin_path = os.path.dirname(os.path.abspath(__file__))
 
     parser = argparse.ArgumentParser(description="Build and package UnrealLibretro plugin.")
-    parser.add_argument("ue_path", type=str, help="Path to an Unreal Engine installation you want to build the plugin for.")
-    
-    ue_path = parser.parse_args().ue_path
+    parser.add_argument("ue_path", help="Path to an Unreal Engine installation you want to build the plugin for.")
+    parser.add_argument("package_path", nargs='?', help="Path where the plugin will be packaged.")
+
+    args = parser.parse_args()
+    ue_path = args.ue_path
+    if args.package_path is None:
+        package_path = plugin_path
+    else:
+        package_path = os.path.abspath(args.package_path) 
 
     major, minor = get_unreal_version(ue_path)
 
@@ -60,13 +64,13 @@ if __name__ == "__main__":
     else:
         uplugin_json = json.load(open(f"UnrealLibretro.uplugin.UE5.3"))
 
-    uplugin_json["VersionName"] = git_get_tag_or_short_hash(revision="HEAD")
+    uplugin_json["VersionName"] = git_get_version_name(revision="HEAD")
     json.dump(uplugin_json, open("UnrealLibretro.uplugin", "w"), indent=4)
 
     status = os.system(
         f'"{ue_path}/Engine/Build/BatchFiles/RunUAT" BuildPlugin -Rocket'
         f' -Plugin={plugin_path}/UnrealLibretro.uplugin -TargetPlatforms=Win64'
-        f' -Package={plugin_path}/UE_{major}.{minor}/UnrealLibretro -VS2019'
+        f' -Package={package_path}/UnrealLibretro-{major}.{minor}/UnrealLibretro -VS2019'
     )
 
     if status:
@@ -77,7 +81,7 @@ if __name__ == "__main__":
         Path(os.path.dirname(path)).mkdir()
         Path(path).touch()
 
-    unix_touch(f'{plugin_path}/UE_{major}.{minor}/UnrealLibretro/MyROMs/Place Your ROMs in this Directory')
-    unix_touch(f'{plugin_path}/UE_{major}.{minor}/UnrealLibretro/MyCores/Place Your Libretro Cores in this Directory')
+    unix_touch(f'{package_path}/UnrealLibretro-{major}.{minor}/UnrealLibretro/MyROMs/Place Your ROMs in this Directory')
+    unix_touch(f'{package_path}/UnrealLibretro-{major}.{minor}/UnrealLibretro/MyCores/Place Your Libretro Cores in this Directory')
 
-    os.system(f'tar -acf UnrealLibretro-{major}.{minor}.zip -C {plugin_path}/UE_{major}.{minor} UnrealLibretro')
+    os.system(f'tar -acf UnrealLibretro-{major}.{minor}.zip -C {package_path}/UnrealLibretro-{major}.{minor} UnrealLibretro')
