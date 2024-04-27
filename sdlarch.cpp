9875a21fea2080759419497189601246fe8ae440
John Rehbein <john.rehbein@icloud.com>
2024-04-26 22:42:36 -0700

    Convert indentation to spaces in sdlarch.cpp

--- sdlarch.cpp
+++ sdlarch.cpp
@@ -50,7 +50,7 @@ bool running = true;
 
 
 static struct {
-	GLuint tex_id;
+    GLuint tex_id;
     GLuint fbo_id;
     GLuint rbo_id;
 
@@ -58,13 +58,13 @@ static struct {
     int glminor;
 
 
-	GLuint pitch;
-	GLint tex_w, tex_h;
-	GLuint clip_w, clip_h;
+    GLuint pitch;
+    GLint tex_w, tex_h;
+    GLuint clip_w, clip_h;
 
-	GLuint pixfmt;
-	GLuint pixtype;
-	GLuint bpp;
+    GLuint pixfmt;
+    GLuint pixtype;
+    GLuint bpp;
 
     struct retro_hw_render_callback hw;
 } g_video  = {0};
@@ -107,32 +107,32 @@ static const char *g_fshader_src =
 
 
 static struct {
-	void *handle;
-	bool initialized;
-	bool supports_no_game;
+    void *handle;
+    bool initialized;
+    bool supports_no_game;
     uint64_t quirks;
-	// The last performance counter registered. TODO: Make it a linked list.
-	struct retro_perf_counter* perf_counter_last;
+    // The last performance counter registered. TODO: Make it a linked list.
+    struct retro_perf_counter* perf_counter_last;
 
-	void (*retro_init)(void);
-	void (*retro_deinit)(void);
-	unsigned (*retro_api_version)(void);
-	void (*retro_get_system_info)(struct retro_system_info *info);
-	void (*retro_get_system_av_info)(struct retro_system_av_info *info);
-	void (*retro_set_controller_port_device)(unsigned port, unsigned device);
-	void (*retro_reset)(void);
-	void (*retro_run)(void);
-	size_t (*retro_serialize_size)(void);
-	bool (*retro_serialize)(void *data, size_t size);
-	bool (*retro_unserialize)(const void *data, size_t size);
-//	void retro_cheat_reset(void);
-//	void retro_cheat_set(unsigned index, bool enabled, const char *code);
-	bool (*retro_load_game)(const struct retro_game_info *game);
-//	bool retro_load_game_special(unsigned game_type, const struct retro_game_info *info, size_t num_info);
-	void (*retro_unload_game)(void);
-//	unsigned retro_get_region(void);
-//	void *retro_get_memory_data(unsigned id);
-//	size_t retro_get_memory_size(unsigned id);
+    void (*retro_init)(void);
+    void (*retro_deinit)(void);
+    unsigned (*retro_api_version)(void);
+    void (*retro_get_system_info)(struct retro_system_info *info);
+    void (*retro_get_system_av_info)(struct retro_system_av_info *info);
+    void (*retro_set_controller_port_device)(unsigned port, unsigned device);
+    void (*retro_reset)(void);
+    void (*retro_run)(void);
+    size_t (*retro_serialize_size)(void);
+    bool (*retro_serialize)(void *data, size_t size);
+    bool (*retro_unserialize)(const void *data, size_t size);
+//  void retro_cheat_reset(void);
+//  void retro_cheat_set(unsigned index, bool enabled, const char *code);
+    bool (*retro_load_game)(const struct retro_game_info *game);
+//  bool retro_load_game_special(unsigned game_type, const struct retro_game_info *info, size_t num_info);
+    void (*retro_unload_game)(void);
+//  unsigned retro_get_region(void);
+//  void *retro_get_memory_data(unsigned id);
+//  size_t retro_get_memory_size(unsigned id);
 } g_retro;
 
 #define UMETA(...)
@@ -293,8 +293,8 @@ static struct FLibretroContext g_libretro_context;
 auto &g_ulnet_session = g_libretro_context.ulnet_session;
 
 struct keymap {
-	unsigned k;
-	unsigned rk;
+    unsigned k;
+    unsigned rk;
 };
 
 static struct keymap g_binds[] = {
@@ -317,7 +317,7 @@ static struct keymap g_binds[] = {
 #define load_sym(V, S) do {\
     if (!((*(void**)&V) = SDL_LoadFunction(g_retro.handle, #S))) \
         SAM2_LOG_FATAL("Failed to load symbol '" #S "'': %s", SDL_GetError()); \
-	} while (0)
+    } while (0)
 #define load_retro_sym(S) load_sym(g_retro.S, S)
 
 static void logical_partition(int sz, int redundant, int *n, int *out_k, int *packet_size, int *packet_groups);
@@ -474,7 +474,7 @@ static void init_framebuffer(int width, int height)
 
 
 static void resize_cb(int w, int h) {
-	glViewport(0, 0, w, h);
+    glViewport(0, 0, w, h);
 }
 
 
@@ -507,7 +507,7 @@ static void create_window(int width, int height) {
 
     g_win = SDL_CreateWindow("sdlarch", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
 
-	if (!g_win)
+    if (!g_win)
         SAM2_LOG_FATAL("Failed to create window: %s", SDL_GetError());
 
     g_ctx = SDL_GL_CreateContext(g_win);
@@ -539,95 +539,95 @@ static void create_window(int width, int height) {
 
 
 static void resize_to_aspect(double ratio, int sw, int sh, int *dw, int *dh) {
-	*dw = sw;
-	*dh = sh;
+    *dw = sw;
+    *dh = sh;
 
-	if (ratio <= 0)
-		ratio = (double)sw / sh;
+    if (ratio <= 0)
+        ratio = (double)sw / sh;
 
-	if ((float)sw / sh < 1)
-		*dw = *dh * ratio;
-	else
-		*dh = *dw / ratio;
+    if ((float)sw / sh < 1)
+        *dw = *dh * ratio;
+    else
+        *dh = *dw / ratio;
 }
 
 
 static void video_configure(const struct retro_game_geometry *geom) {
-	int nwidth, nheight;
+    int nwidth, nheight;
 
-	resize_to_aspect(geom->aspect_ratio, geom->base_width * 1, geom->base_height * 1, &nwidth, &nheight);
+    resize_to_aspect(geom->aspect_ratio, geom->base_width * 1, geom->base_height * 1, &nwidth, &nheight);
 
-	nwidth *= g_scale;
-	nheight *= g_scale;
+    nwidth *= g_scale;
+    nheight *= g_scale;
 
-	if (!g_win)
-		create_window(nwidth, nheight);
+    if (!g_win)
+        create_window(nwidth, nheight);
 
-	if (g_video.tex_id)
-		glDeleteTextures(1, &g_video.tex_id);
+    if (g_video.tex_id)
+        glDeleteTextures(1, &g_video.tex_id);
 
-	g_video.tex_id = 0;
+    g_video.tex_id = 0;
 
-	if (!g_video.pixfmt)
-		g_video.pixfmt = GL_UNSIGNED_SHORT_5_5_5_1;
+    if (!g_video.pixfmt)
+        g_video.pixfmt = GL_UNSIGNED_SHORT_5_5_5_1;
 
     SDL_SetWindowSize(g_win, nwidth, nheight);
 
-	glGenTextures(1, &g_video.tex_id);
+    glGenTextures(1, &g_video.tex_id);
 
-	if (!g_video.tex_id)
-		SAM2_LOG_FATAL("Failed to create the video texture");
+    if (!g_video.tex_id)
+        SAM2_LOG_FATAL("Failed to create the video texture");
 
-	g_video.pitch = geom->max_width * g_video.bpp;
+    g_video.pitch = geom->max_width * g_video.bpp;
 
-	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
+    glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
 
 //	glPixelStorei(GL_UNPACK_ALIGNMENT, s_video.pixfmt == GL_UNSIGNED_INT_8_8_8_8_REV ? 4 : 2);
 //	glPixelStorei(GL_UNPACK_ROW_LENGTH, s_video.pitch / s_video.bpp);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, geom->max_width, geom->max_height, 0,
-			g_video.pixtype, g_video.pixfmt, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, geom->max_width, geom->max_height, 0,
+            g_video.pixtype, g_video.pixfmt, NULL);
 
-	glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
     init_framebuffer(geom->max_width, geom->max_height);
 
-	g_video.tex_w = geom->max_width;
-	g_video.tex_h = geom->max_height;
-	g_video.clip_w = geom->base_width;
-	g_video.clip_h = geom->base_height;
+    g_video.tex_w = geom->max_width;
+    g_video.tex_h = geom->max_height;
+    g_video.clip_w = geom->base_width;
+    g_video.clip_h = geom->base_height;
 
-	refresh_vertex_data();
+    refresh_vertex_data();
 
     g_video.hw.context_reset();
 }
 
 
 static bool video_set_pixel_format(unsigned format) {
-	switch (format) {
-	case RETRO_PIXEL_FORMAT_0RGB1555:
-		g_video.pixfmt = GL_UNSIGNED_SHORT_5_5_5_1;
-		g_video.pixtype = GL_BGRA;
-		g_video.bpp = sizeof(uint16_t);
-		break;
-	case RETRO_PIXEL_FORMAT_XRGB8888:
-		g_video.pixfmt = GL_UNSIGNED_INT_8_8_8_8_REV;
-		g_video.pixtype = GL_BGRA;
-		g_video.bpp = sizeof(uint32_t);
-		break;
-	case RETRO_PIXEL_FORMAT_RGB565:
-		g_video.pixfmt  = GL_UNSIGNED_SHORT_5_6_5;
-		g_video.pixtype = GL_RGB;
-		g_video.bpp = sizeof(uint16_t);
-		break;
-	default:
-		SAM2_LOG_FATAL("Unknown pixel type %u", format);
-	}
+    switch (format) {
+    case RETRO_PIXEL_FORMAT_0RGB1555:
+        g_video.pixfmt = GL_UNSIGNED_SHORT_5_5_5_1;
+        g_video.pixtype = GL_BGRA;
+        g_video.bpp = sizeof(uint16_t);
+        break;
+    case RETRO_PIXEL_FORMAT_XRGB8888:
+        g_video.pixfmt = GL_UNSIGNED_INT_8_8_8_8_REV;
+        g_video.pixtype = GL_BGRA;
+        g_video.bpp = sizeof(uint32_t);
+        break;
+    case RETRO_PIXEL_FORMAT_RGB565:
+        g_video.pixfmt  = GL_UNSIGNED_SHORT_5_6_5;
+        g_video.pixtype = GL_RGB;
+        g_video.bpp = sizeof(uint16_t);
+        break;
+    default:
+        SAM2_LOG_FATAL("Unknown pixel type %u", format);
+    }
 
-	return true;
+    return true;
 }
 
 #define FORMAT_UNIT_COUNT_SIZE 64
@@ -1566,23 +1566,23 @@ static void draw_core_frame() {
 static void video_refresh(const void *data, unsigned width, unsigned height, unsigned pitch) {
     if (g_video.clip_w != width || g_video.clip_h != height)
     {
-		g_video.clip_h = height;
-		g_video.clip_w = width;
+        g_video.clip_h = height;
+        g_video.clip_w = width;
 
-		refresh_vertex_data();
-	}
+        refresh_vertex_data();
+    }
 
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
-	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
+    glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
 
-	if (pitch != g_video.pitch)
-		g_video.pitch = pitch;
+    if (pitch != g_video.pitch)
+        g_video.pitch = pitch;
 
     if (data && data != RETRO_HW_FRAME_BUFFER_VALID) {
         glPixelStorei(GL_UNPACK_ROW_LENGTH, g_video.pitch / g_video.bpp);
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
-						g_video.pixtype, g_video.pixfmt, data);
-	}
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
+                        g_video.pixtype, g_video.pixfmt, data);
+    }
 }
 
 static void video_deinit() {
@@ -1602,7 +1602,7 @@ static void video_deinit() {
         glDeleteProgram(g_shader.program);
 
     g_video.fbo_id = 0;
-	g_video.tex_id = 0;
+    g_video.tex_id = 0;
     g_shader.vao = 0;
     g_shader.vbo = 0;
     g_shader.program = 0;
@@ -1656,16 +1656,16 @@ static size_t audio_write(const int16_t *buf, unsigned frames) {
 
 
 static void core_log(enum retro_log_level level, const char *fmt, ...) {
-	char buffer[4096] = {0};
-	static const char * levelstr[] = { "dbg", "inf", "wrn", "err" };
+    char buffer[4096] = {0};
+    static const char * levelstr[] = { "dbg", "inf", "wrn", "err" };
 
-	va_list va;
-	va_start(va, fmt);
-	vsnprintf(buffer, sizeof(buffer), fmt, va);
-	va_end(va);
+    va_list va;
+    va_start(va, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, va);
+    va_end(va);
 
     if (level < g_log_level)
-		return;
+        return;
 
     // This is duplicated code wrt SAM2_LOG_DEBUG, SAM2_LOG_INFO, etc. but it would be tricky to do a refactor
     time_t t = time(NULL);
@@ -1784,7 +1784,7 @@ static void core_perf_log() {
 }
 
 static bool core_environment(unsigned cmd, void *data) {
-	switch (cmd) {
+    switch (cmd) {
     case RETRO_ENVIRONMENT_SET_VARIABLES: {
         const struct retro_variable *vars = (const struct retro_variable *)data;
         size_t num_vars = 0;
@@ -1849,11 +1849,11 @@ static bool core_environment(unsigned cmd, void *data) {
         g_ulnet_session.flags &= ~ULNET_SESSION_FLAG_CORE_OPTIONS_DIRTY;
         return true;
     }
-	case RETRO_ENVIRONMENT_GET_LOG_INTERFACE: {
-		struct retro_log_callback *cb = (struct retro_log_callback *)data;
-		cb->log = core_log;
+    case RETRO_ENVIRONMENT_GET_LOG_INTERFACE: {
+        struct retro_log_callback *cb = (struct retro_log_callback *)data;
+        cb->log = core_log;
         return true;
-	}
+    }
     case RETRO_ENVIRONMENT_GET_PERF_INTERFACE: {
         struct retro_perf_callback *perf = (struct retro_perf_callback *)data;
         perf->get_time_usec = cpu_features_get_time_usec;
@@ -1865,19 +1865,19 @@ static bool core_environment(unsigned cmd, void *data) {
         perf->perf_log = core_perf_log;
         return true;
     }
-	case RETRO_ENVIRONMENT_GET_CAN_DUPE: {
-		bool *bval = (bool*)data;
-		*bval = true;
+    case RETRO_ENVIRONMENT_GET_CAN_DUPE: {
+        bool *bval = (bool*)data;
+        *bval = true;
         return true;
     }
-	case RETRO_ENVIRONMENT_SET_PIXEL_FORMAT: {
-		const enum retro_pixel_format *fmt = (enum retro_pixel_format *)data;
+    case RETRO_ENVIRONMENT_SET_PIXEL_FORMAT: {
+        const enum retro_pixel_format *fmt = (enum retro_pixel_format *)data;
 
-		if (*fmt > RETRO_PIXEL_FORMAT_RGB565)
-			return false;
+        if (*fmt > RETRO_PIXEL_FORMAT_RGB565)
+            return false;
 
-		return video_set_pixel_format(*fmt);
-	}
+        return video_set_pixel_format(*fmt);
+    }
     case RETRO_ENVIRONMENT_SET_HW_RENDER: {
         struct retro_hw_render_callback *hw = (struct retro_hw_render_callback*)data;
         hw->get_current_framebuffer = core_get_current_framebuffer;
@@ -1935,10 +1935,10 @@ static bool core_environment(unsigned cmd, void *data) {
         g_retro.quirks = *quirks;
         return true;
     }
-	default:
-		core_log(RETRO_LOG_DEBUG, "Unhandled env #%u", cmd);
-		return false;
-	}
+    default:
+        core_log(RETRO_LOG_DEBUG, "Unhandled env #%u", cmd);
+        return false;
+    }
 
     return false;
 }
@@ -2006,70 +2006,70 @@ int16_t FLibretroContext::core_input_state(unsigned port, unsigned device, unsig
 }
 
 static int16_t core_input_state(unsigned port, unsigned device, unsigned index, unsigned id) {
-	return g_libretro_context.core_input_state(port, device, index, id);
+    return g_libretro_context.core_input_state(port, device, index, id);
 }
 
 
 static void core_audio_sample(int16_t left, int16_t right) {
-	int16_t buf[2] = {left, right};
-	audio_write(buf, 1);
+    int16_t buf[2] = {left, right};
+    audio_write(buf, 1);
 }
 
 
 static size_t core_audio_sample_batch(const int16_t *data, size_t frames) {
-	return audio_write(data, frames);
+    return audio_write(data, frames);
 }
 
 
 static void core_load(const char *sofile) {
-	void (*set_environment)(retro_environment_t) = NULL;
-	void (*set_video_refresh)(retro_video_refresh_t) = NULL;
-	void (*set_input_poll)(retro_input_poll_t) = NULL;
-	void (*set_input_state)(retro_input_state_t) = NULL;
-	void (*set_audio_sample)(retro_audio_sample_t) = NULL;
-	void (*set_audio_sample_batch)(retro_audio_sample_batch_t) = NULL;
-	memset(&g_retro, 0, sizeof(g_retro));
+    void (*set_environment)(retro_environment_t) = NULL;
+    void (*set_video_refresh)(retro_video_refresh_t) = NULL;
+    void (*set_input_poll)(retro_input_poll_t) = NULL;
+    void (*set_input_state)(retro_input_state_t) = NULL;
+    void (*set_audio_sample)(retro_audio_sample_t) = NULL;
+    void (*set_audio_sample_batch)(retro_audio_sample_batch_t) = NULL;
+    memset(&g_retro, 0, sizeof(g_retro));
     g_retro.handle = SDL_LoadObject(sofile);
 
-	if (!g_retro.handle)
+    if (!g_retro.handle)
         SAM2_LOG_FATAL("Failed to load core: %s", SDL_GetError());
 
-	load_retro_sym(retro_init);
-	load_retro_sym(retro_deinit);
-	load_retro_sym(retro_api_version);
-	load_retro_sym(retro_get_system_info);
-	load_retro_sym(retro_get_system_av_info);
-	load_retro_sym(retro_set_controller_port_device);
-	load_retro_sym(retro_reset);
-	load_retro_sym(retro_run);
-	load_retro_sym(retro_load_game);
-	load_retro_sym(retro_unload_game);
+    load_retro_sym(retro_init);
+    load_retro_sym(retro_deinit);
+    load_retro_sym(retro_api_version);
+    load_retro_sym(retro_get_system_info);
+    load_retro_sym(retro_get_system_av_info);
+    load_retro_sym(retro_set_controller_port_device);
+    load_retro_sym(retro_reset);
+    load_retro_sym(retro_run);
+    load_retro_sym(retro_load_game);
+    load_retro_sym(retro_unload_game);
     load_retro_sym(retro_serialize_size);
     load_retro_sym(retro_serialize);
     load_retro_sym(retro_unserialize);
 
-	load_sym(set_environment, retro_set_environment);
-	load_sym(set_video_refresh, retro_set_video_refresh);
-	load_sym(set_input_poll, retro_set_input_poll);
-	load_sym(set_input_state, retro_set_input_state);
-	load_sym(set_audio_sample, retro_set_audio_sample);
-	load_sym(set_audio_sample_batch, retro_set_audio_sample_batch);
+    load_sym(set_environment, retro_set_environment);
+    load_sym(set_video_refresh, retro_set_video_refresh);
+    load_sym(set_input_poll, retro_set_input_poll);
+    load_sym(set_input_state, retro_set_input_state);
+    load_sym(set_audio_sample, retro_set_audio_sample);
+    load_sym(set_audio_sample_batch, retro_set_audio_sample_batch);
 
-	set_environment(core_environment);
-	set_video_refresh(core_video_refresh);
-	set_input_poll(core_input_poll);
-	set_input_state(core_input_state);
-	set_audio_sample(core_audio_sample);
-	set_audio_sample_batch(core_audio_sample_batch);
+    set_environment(core_environment);
+    set_video_refresh(core_video_refresh);
+    set_input_poll(core_input_poll);
+    set_input_state(core_input_state);
+    set_audio_sample(core_audio_sample);
+    set_audio_sample_batch(core_audio_sample_batch);
 
-	g_retro.retro_init();
-	g_retro.initialized = true;
+    g_retro.retro_init();
+    g_retro.initialized = true;
 
-	SAM2_LOG_INFO("Core loaded");
+    SAM2_LOG_INFO("Core loaded");
 }
 
 static void core_load_game(const char *filename) {
-	struct retro_game_info info = { filename, 0 };
+    struct retro_game_info info = { filename, 0 };
 
     info.path = filename;
     info.meta = "";
@@ -2104,13 +2104,13 @@ static void core_load_game(const char *filename) {
         }
     }
 
-	if (!g_retro.retro_load_game(&info))
-		SAM2_LOG_FATAL("The core failed to load the content.");
+    if (!g_retro.retro_load_game(&info))
+        SAM2_LOG_FATAL("The core failed to load the content.");
 
-	g_retro.retro_get_system_av_info(&g_av);
+    g_retro.retro_get_system_av_info(&g_av);
 
-	video_configure(&g_av.geometry);
-	audio_init(g_av.timing.sample_rate);
+    video_configure(&g_av.geometry);
+    audio_init(g_av.timing.sample_rate);
 
     if (info.data)
         SDL_free((void*)info.data);
@@ -2124,10 +2124,10 @@ static void core_load_game(const char *filename) {
 }
 
 static void core_unload() {
-	if (g_retro.initialized)
-		g_retro.retro_deinit();
+    if (g_retro.initialized)
+        g_retro.retro_deinit();
 
-	if (g_retro.handle)
+    if (g_retro.handle)
         SDL_UnloadObject(g_retro.handle);
 }
 
@@ -2332,8 +2332,8 @@ int main(int argc, char *argv[]) {
     g_argc = argc;
     g_argv = argv;
 
-	if (argc < 2)
-		SAM2_LOG_FATAL("usage: %s <core> [game]", argv[0]);
+    if (argc < 2)
+        SAM2_LOG_FATAL("usage: %s <core> [game]", argv[0]);
 
     if (   strcmp(g_sam2_address, "localhost")
         || strcmp(g_sam2_address, "127.0.0.1")
