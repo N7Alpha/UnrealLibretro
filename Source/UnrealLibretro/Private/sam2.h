b43120f6c8ca68730e6c734b9189a2059d0f929c
John Rehbein <john.rehbein@icloud.com>
2026-06-19 17:08:33 -0700

    Remove todo

--- Source/UnrealLibretro/Private/sam2.h
+++ Source/UnrealLibretro/Private/sam2.h
@@ -122,7 +122,6 @@
 #define SAM2_SPECTATOR_START (SAM2_PORT_MAX + 1)
 
 // All data is sent in little-endian format
-// All strings are utf-8 encoded unless stated otherwise... @todo Actually I should just add _utf8 if the field isn't ascii
 // Packing of structs is asserted at compile time since packing directives are compiler specific
 typedef struct sam2_room {
     char name[64];
